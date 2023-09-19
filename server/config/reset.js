776feae6b0fd4321b55bfbb90dc0e75b7acdfcce
Ethan Pineda <joshuapineda66@gmail.com>
2023-09-19 13:32:10 -0500

    conneted to db and added starter date

---
+++ server/config/reset.js
@@ -0,0 +1,55 @@
+import { pool } from "./database.js";   
+import "./dotenv.js"; 
+import resources from "../data/resources.js"; 
+
+const createResourcesTable = async () => {
+
+    const createTableQuery = `
+    
+        CREATE TABLE IF NOT EXISTS resources (
+
+            id SERIAL PRIMARY KEY, 
+            name VARCHAR(255) NOT NULL, 
+            website VARCHAR(255) NOT NULL,
+            about TEXT NOT NULL,
+            phone VARCHAR(100) NOT NULL
+        )
+    `;
+
+    try {
+        const response = await pool.query(createTableQuery);
+        console.log("Resources table created successfully!: ", response);  
+    } catch (error) {
+        console.log("Error creating table: ", error);
+        pool.end(); 
+    };
+};
+
+const seedResourcesTable = async () => {
+
+    await createResourcesTable(); 
+
+    resources.forEach((resource) => {
+
+        const insertQuery = {
+            text: `INSERT INTO resources (name, website, about, phone) VALUES ($1, $2, $3, $4)`,
+        };
+
+        const values = [
+            resource.name,
+            resource.website,
+            resource.about,
+            resource.phone
+        ];
+
+        pool.query(insertQuery, values, (error, res) => {
+            if (error) {
+                console.log("Error inserting into table: ", error);
+            } else {
+                console.log(`✅ ${resource.name} added successfully`); 
+            };
+        })
+    })
+};
+
+seedResourcesTable(); 
